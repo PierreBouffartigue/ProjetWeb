d28d08e43d58b73220183be8488b95aad9082602
PierreBouffartigue <43409971+PierreBouffartigue@users.noreply.github.com>
2020-04-13 22:09:13 +0200

    Update page vote

--- Epicka/resources/views/vote.blade.php
+++ Epicka/resources/views/vote.blade.php
@@ -10,13 +10,11 @@
                                     <p>Vote pour le serveur et gagne des récompenses :</p>
                                     <form action="#">
                                         <div class="mt-10">
-                                            <input type="text" name="Pseudo" placeholder="Pseudo"
-                                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Pseudo'" required
-                                                class="single-input">
+                                            <input type="text" name='pseudo'/>
                                         </div>
                                         <br><form action="#" method="POST">
-                                            <a type="submit" target="_blank" class="btn btn-info">Voter</a><br>
-                                        </form><br><br>
+                                            <a type="submit" href="https://www.rpg-paradize.com/?page=vote&vote=112582" target="_blank" class="btn btn-info">Voter</a><br>
+                                    </form><br><br>
                                     <br><br><p>Une fois en jeu utilise /reward pour récupérer tes récompenses !</p>
                                 </div>
                             </div>
